7efeedf5622447d0e4e7b5cc2333b6b7e04ffbfe
SipjakiBlora <sipjakiblora@gmail.com>
2025-11-26 05:28:41 +0700

    erg

--- resources/views/backend/00_administrator/00_baganterpisah/01_header.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/01_header.blade.php
@@ -63,7 +63,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 
-    <link rel="icon" href="/assets/icon/logokabupatenblora.png" type="image/x-icon">
+    <link rel="icon" href="/storage/logo/sipjakikbb.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
